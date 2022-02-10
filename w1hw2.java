1c224c201822a85f5e81690898dcddd5f402330a
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2022-02-10 13:44:40 -0600

    first question down

--- w1hw2.java
+++ w1hw2.java
@@ -1,32 +1,51 @@
 package m3w1HW2;
 
+import java.util.ArrayList;
+
 public class w1hw2 {
 	
-//	Write a Java program to copy one array list into another
-//
-//	 Write a Java program to extract a portion of an array list
-//
-//	Write a Java program of swap two elements in an array list
-//
-//	Write a Java program to test an array list is empty or not
-//
-//	Write a Java program to replace the second element of an ArrayList with the specified element.
-//
-//	Write a Java program to trim the capacity of an array list the current list size
-//
-//	 Write a Java program to test a hash set is empty or not.
-//
-//	Write a Java program to get the number of elements in a hash set
-//
-//	Write a Java program to iterate through all elements in a hash list.
-//
-//	Write a Java program to convert a hash set to an array.
-//
-//	Write a Java program to compare two sets and retain elements that are the same on both sets.
-
 	public static void main(String[] args) {
-		// 
+		//	Write a Java program to copy one array list into another
+		ArrayList<String> anime = new ArrayList<>();
+		anime.add("BnHA");
+		anime.add("Fairy Tail");
+		anime.add("Jujutsu Kaisen");
+		
+		ArrayList<String> anime2 = new ArrayList<>();
+		anime2.add("Demon Slayer");
+		anime2.add("Yu-Yu Hakusho");
+		
+		ArrayList<String> anime3 = new ArrayList<>();
+		anime3.add("JoJo");
+		anime3.addAll(anime);
+		anime3.addAll(anime2);
+//		System.out.println(anime);
+//		System.out.println(anime2);
+		System.out.println(anime3);
+		
+//		 Write a Java program to extract a portion of an array list
+		
+		
 
+//			Write a Java program to swap two elements in an array list
+
+//			Write a Java program to if test an array list is empty or not
+
+//			Write a Java program to replace the second element of an ArrayList with the specified element.
+
+//			Write a Java program to trim the capacity of an array list the current list size
+		
+//			 Write a Java program to test if a hash set is empty or not.
+		
+//			Write a Java program to get the number of elements in a hash set
+		
+//			Write a Java program to iterate through all elements in a hash list.
+		
+//			Write a Java program to convert a hash set to an array.
+		
+//			Write a Java program to compare two sets and retain elements that are the same on both sets.
+		
+		
 	}
 
 }
