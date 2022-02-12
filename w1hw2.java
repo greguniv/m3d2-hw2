49d4a09d092d48d43ac9a728a38878dcd805a95b
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2022-02-12 12:30:11 -0600

    question 3

--- w1hw2.java
+++ w1hw2.java
@@ -1,8 +1,12 @@
 package m3w1HW2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class w1hw2 {
+	
 	
 	public static void main(String[] args) {
 		//	Write a Java program to copy one array list into another
@@ -10,30 +14,40 @@ public class w1hw2 {
 		anime.add("BnHA");
 		anime.add("Fairy Tail");
 		anime.add("Jujutsu Kaisen");
+		System.out.println(anime);
 		
 		ArrayList<String> anime2 = new ArrayList<>();
 		anime2.add("Demon Slayer");
 		anime2.add("Yu-Yu Hakusho");
+		System.out.println(anime2);
+		
 		
 		ArrayList<String> anime3 = new ArrayList<>();
 		anime3.add("JoJo");
 		anime3.addAll(anime);
 		anime3.addAll(anime2);
-//		System.out.println(anime);
-//		System.out.println(anime2);
 		System.out.println(anime3);
 		
 //		 Write a Java program to extract a portion of an array list
-		
-		
-
+		List<String> extract = anime3.subList(2, 5);
+		System.out.println("The original " + anime3);
+		System.out.println("This is the extracted " + extract);
+				
 //			Write a Java program to swap two elements in an array list
+		Collections.swap(anime3, 2, 5);
+		System.out.println("The Swap: " + anime3);
+		
 
-//			Write a Java program to if test an array list is empty or not
+//			Write a Java program to test if an array list is empty or not
+		
 
 //			Write a Java program to replace the second element of an ArrayList with the specified element.
 
 //			Write a Java program to trim the capacity of an array list the current list size
+		
+		
+		
+		//HUNGRY FOR MORE
 		
 //			 Write a Java program to test if a hash set is empty or not.
 		
